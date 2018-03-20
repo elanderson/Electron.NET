9f43e55868fd2d023b81c12645fa9125dcd5286b
Eric Anderson <elanderson@gmail.com>
2018-03-20 06:44:31 -0500

    Add model

---
+++ src/ElectronTest/Models/Contact.cs
@@ -0,0 +1,14 @@
+namespace ElectronTest.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Subregion { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
